0e2dc2f3f3a8ee17ede176ac12f30914a4a61356
Kushagra Gour <chinchang457@gmail.com>
2015-04-03 17:21:35 +0530

    add builds.

--- dist/cta.js
+++ dist/cta.js
@@ -1,4 +1,4 @@
-/*! cta.js - v0.1.0 - 2015-03-30
+/*! cta.js - v0.2.0 - 2015-04-03
 * http://kushagragour.in/lab/ctajs/
 * Copyright (c) 2015 Kushagra Gour; Licensed MIT */
 
@@ -77,11 +77,13 @@
 
 		// Whether to position the dummy animating element relative to window (fixed positioned) or not.
 		relativeToWindow: false
-	}
+	};
 
 	function cta(trigger, target, options, callback) {
 		if (!isSupportedBrowser) {
-			callback && callback(target);
+			if (callback) {
+				callback(target);
+			}
 			return;
 		}
 
@@ -141,24 +143,27 @@
 		dummy.style.setProperty('transform', 'translate(' + diffX + 'px, ' + diffY + 'px) scale(' + scaleXRatio + ', ' + scaleYRatio + ')', 'important');
 		document.body.appendChild(dummy);
 
-		// We start animation on the next available frame.
-		requestAnimationFrame(function () {
-			dummy.style.setProperty('background', targetBackground, 'important');
+		// Trigger a layout to let styles apply.
+		var justReadIt = dummy.offsetTop;
 
-			// Remove the reverse transforms to get the dummy transition back to its normal/final state.
-			dummy.style.removeProperty('transform');
+		// We reverting everything to lets things animate.
+		dummy.style.setProperty('background', targetBackground, 'important');
 
-			dummy.addEventListener('transitionend', function transitionEndCallback() {
-				dummy.removeEventListener('transitionend', transitionEndCallback);
+		// Remove the reverse transforms to get the dummy transition back to its normal/final state.
+		dummy.style.removeProperty('transform');
 
-				callback && callback(target);
-				// Animate the dummy element to zero opacity while the target is getting rendered.
-				dummy.style.transitionDuration = (options.targetShowDuration + extraTransitionDuration) + 's';
-				dummy.style.opacity = 0;
-				setTimeout(function () {
-					dummy.parentNode.removeChild(dummy);
-				}, (options.targetShowDuration + extraTransitionDuration) * 1000);
-			});
+		dummy.addEventListener('transitionend', function transitionEndCallback() {
+			dummy.removeEventListener('transitionend', transitionEndCallback);
+
+			if (callback) {
+				callback(target);
+			}
+			// Animate the dummy element to zero opacity while the target is getting rendered.
+			dummy.style.transitionDuration = (options.targetShowDuration + extraTransitionDuration) + 's';
+			dummy.style.opacity = 0;
+			setTimeout(function () {
+				dummy.parentNode.removeChild(dummy);
+			}, (options.targetShowDuration + extraTransitionDuration) * 1000);
 		});
 
 		// Return a reverse animation function for the called animation.
